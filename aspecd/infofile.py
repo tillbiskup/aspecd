a1d992a2a914a220bdffc8830c3e1809b0630c6e
Till Biskup <till@till-biskup.de>
2017-04-20 18:23:37 +0200

    Module for handling info files

---
+++ aspecd/infofile.py
@@ -0,0 +1,137 @@
+"""
+Handle infofile files containing meta data for (experimental) data.
+
+The infofile format is a special format dedicated to storing meta data
+accompanying experimental data. Further information can be found online:
+
+http://till-biskup.de/en/software/info/format
+
+This module reads and parses files complying with the infofile file format.
+
+Further plans are to include functionality to map the information contained
+in the info file to datasets.
+"""
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class InfofileTypeError(Error):
+    """Exception raised for wrong file format.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message=''):
+        self.message = message
+
+
+class InfofileEmptyError(Error):
+    """Exception raised for empty file.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message=''):
+        self.message = message
+
+
+class Infofile:
+
+    def __init__(self, filename=None):
+        self.parameters = {}
+        self.filename = filename
+
+        self._file_contents = []
+        self._comment_char = '%'
+        self._escape_char = '\\'
+
+    def parse(self):
+        self._file_contents = self._read()
+
+        if not self._file_contents:
+            raise InfofileEmptyError
+
+        if not self._is_infofile():
+            raise InfofileTypeError
+
+        blockname = ''
+        key = ''
+        value = ''
+        tmp_block = {}
+
+        for line in self._file_contents[1:]:
+            if self._is_comment_block(blockname):
+                if blockname not in self.parameters:
+                    self.parameters[blockname] = []
+                self.parameters[blockname].append(line)
+                continue
+            if self._is_comment_line(line):
+                continue
+            line = self._remove_inline_comment(line)
+            if self._is_continuation_line(line):
+                if key:
+                    tmp_block[key] = " ".join([value, line.strip()])
+                continue
+            line = line.strip()
+            if not line:
+                if blockname:
+                    self.parameters[blockname] = tmp_block
+                    blockname = ''
+                    tmp_block = {}
+                continue
+            if not blockname:
+                blockname = line.strip()
+                continue
+            [key, value] = [i.strip() for i in line.split(':', maxsplit=1)]
+            tmp_block[key] = value
+
+        # In case last block has not been assigned
+        # (file didn't end with COMMENT block or empty line)
+        if blockname and not self._is_comment_block(blockname):
+            self.parameters[blockname] = tmp_block
+
+    def _is_infofile(self):
+        return 'info file' in self._file_contents[0].lower()
+
+    def _read(self):
+        import os
+        if not self.filename:
+            raise FileExistsError
+        if not os.path.exists(self.filename):
+            raise FileNotFoundError
+        with open(self.filename) as f:
+            file_contents = list(f)
+        return file_contents
+
+    def _is_comment_line(self, line):
+        return line.strip().startswith(self._comment_char)
+
+    def _remove_inline_comment(self, line):
+        # Get all occurrences (positions) of COMMENTCHAR in line
+        occur = [i for i, j in enumerate(line) if j == self._comment_char]
+        # Remove those occurrences prepended with ESCAPE character
+        occur = [i for i in occur if line[i - 1] != self._escape_char]
+        # If there is an inline comment, remove it from line
+        if occur:
+            line = line[:occur[0]]
+        # Replace escaped comment char with comment char
+        return line.replace("".join([self._escape_char, self._comment_char]),
+                            self._comment_char)
+
+    def _is_continuation_line(self, line):
+        import string
+        return line[0] in string.whitespace and line.strip()
+
+    def _is_comment_block(self, blockname):
+        return blockname.lower() == 'comment'
+
+
+def parse(filename=''):
+    ifile = Infofile(filename)
+    ifile.parse()
+    return ifile.parameters
---
+++ tests/test_infofile.py
@@ -0,0 +1,204 @@
+"""Tests for infofile."""
+
+import unittest
+
+from aspecd import infofile
+
+
+class TestInfofileParser(unittest.TestCase):
+
+    def setUp(self):
+        self.ifile = infofile.Infofile()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_has_parameters_property(self):
+        self.assertTrue(hasattr(self.ifile, 'parameters'))
+
+    def test_parameters_is_dict(self):
+        self.assertTrue(isinstance(self.ifile.parameters, dict))
+
+    def test_has_filename_property(self):
+        self.assertTrue(hasattr(self.ifile, 'filename'))
+
+    def test_constructor_sets_filename(self):
+        ifile = infofile.Infofile('test')
+        self.assertEqual(ifile.filename, 'test')
+
+
+class TestInfofileParserParse(unittest.TestCase):
+
+    def write_list_to_file(self, contents, filename):
+        with open(filename, 'w') as f:
+            for line in contents:
+                f.write(line)
+
+    def setUp(self):
+        self.ifile = infofile.Infofile('test.info')
+
+    def tearDown(self):
+        import os
+        if os.path.isfile('test.info'):
+            os.remove('test.info')
+
+    def test_parse_fails_without_filename(self):
+        self.ifile.filename = ''
+        with self.assertRaises(FileExistsError):
+            self.ifile.parse()
+
+    def test_parse_fails_with_nonexisting_file(self):
+        self.ifile.filename = 'nonexisting.info'
+        with self.assertRaises(FileNotFoundError):
+            self.ifile.parse()
+
+    def test_parse_fails_with_empty_file(self):
+        self.write_list_to_file([], self.ifile.filename)
+        with self.assertRaises(infofile.InfofileEmptyError):
+            self.ifile.parse()
+
+    def test_parse_fails_if_not_infofile(self):
+        self.write_list_to_file([' '], self.ifile.filename)
+        with self.assertRaises(infofile.InfofileTypeError):
+            self.ifile.parse()
+
+    def test_parse_minimal_file(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'GENERAL\n'
+            'bla: blub\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertTrue('GENERAL' in self.ifile.parameters)
+        self.assertTrue('bla' in self.ifile.parameters['GENERAL'])
+        self.assertEqual(self.ifile.parameters['GENERAL']['bla'], 'blub')
+
+    def test_parse_different_minimal_file(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'SAMPLE\n'
+            'foo: bar\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertTrue('SAMPLE' in self.ifile.parameters)
+        self.assertTrue('foo' in self.ifile.parameters['SAMPLE'])
+        self.assertEqual(self.ifile.parameters['SAMPLE']['foo'], 'bar')
+
+    def test_parse_inline_comment(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'SAMPLE\n'
+            'foo: bar % some comment\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertEqual(self.ifile.parameters['SAMPLE']['foo'], 'bar')
+
+    def test_parse_escaped_comment_character(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'SAMPLE\n'
+            'foo: bar \%\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertEqual(self.ifile.parameters['SAMPLE']['foo'], 'bar %')
+
+    def test_parse_comment_line(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            '% Some comment line\n'
+            'SAMPLE\n'
+            'foo: bar\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertTrue('SAMPLE' in self.ifile.parameters)
+        self.assertTrue('foo' in self.ifile.parameters['SAMPLE'])
+        self.assertEqual(self.ifile.parameters['SAMPLE']['foo'], 'bar')
+
+    def test_parse_continuation_line(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'SAMPLE\n'
+            'foo: bar\n'
+            ' bla blub'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertTrue('SAMPLE' in self.ifile.parameters)
+        self.assertTrue('foo' in self.ifile.parameters['SAMPLE'])
+        self.assertEqual(self.ifile.parameters['SAMPLE']['foo'],
+                         'bar bla blub')
+
+    def test_parse_file_with_comment_block(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'GENERAL\n'
+            'bla: blub\n'
+            '\n'
+            'COMMENT\n'
+            'And here some comment without colon in line\n'
+        ]
+        self.write_list_to_file(file_content, self.ifile.filename)
+        self.ifile.parse()
+        self.assertTrue('COMMENT' in self.ifile.parameters)
+        self.assertEqual(self.ifile.parameters['COMMENT'],
+                         ['And here some comment without colon in line\n'])
+
+
+class TestInfofileParserNonOOP(unittest.TestCase):
+
+    def write_list_to_file(self, contents, filename):
+        with open(filename, 'w') as f:
+            for line in contents:
+                f.write(line)
+
+    def tearDown(self):
+        import os
+        if os.path.isfile('test.info'):
+            os.remove('test.info')
+
+    def test_parse_fails_without_filename(self):
+        with self.assertRaises(FileExistsError):
+            infofile.parse()
+
+    def test_parse_fails_with_nonexisting_file(self):
+        with self.assertRaises(FileNotFoundError):
+            infofile.parse('nonexisting.info')
+
+    def test_parse_fails_with_empty_file(self):
+        self.write_list_to_file([], 'test.info')
+        with self.assertRaises(infofile.InfofileEmptyError):
+            infofile.parse('test.info')
+
+    def test_parse_fails_if_not_infofile(self):
+        self.write_list_to_file([' '], 'test.info')
+        with self.assertRaises(infofile.InfofileTypeError):
+            infofile.parse('test.info')
+
+    def test_parse_minimal_file(self):
+        file_content = [
+            'test Info File\n'
+            '\n'
+            'GENERAL\n'
+            'bla: blub\n'
+        ]
+        self.write_list_to_file(file_content, 'test.info')
+        parameters = infofile.parse('test.info')
+        self.assertTrue('GENERAL' in parameters)
+        self.assertTrue('bla' in parameters['GENERAL'])
+        self.assertEqual(parameters['GENERAL']['bla'], 'blub')
+
+
+if __name__ == '__main__':
+    unittest.main()
