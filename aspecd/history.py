a86de36d62ec659d1b191969b664880872d5b187
Till Biskup <till@till-biskup.de>
2017-08-06 16:13:25 +0200

    Added docstrings

--- aspecd/history.py
+++ aspecd/history.py
@@ -6,6 +6,18 @@ from datetime import datetime
 
 
 class HistoryRecord:
+    """
+    Generic base class for all classes that can eventually operate on datasets.
+
+    For all classes operating on datasets, such as ProcessingStep, AnalysisStep
+    and others, there exist at least two "representations": (i) the generic one
+    not (necessarily) tied to any concrete dataset, thus portable, and (ii) a
+    concrete one having operated on a dataset and thus being accompanied with
+    information about who has done what when how to what dataset.
+
+    For this second type, a history class derived from HistoryRecord gets used,
+    and it is this second type that is stored inside the Dataset object.
+    """
 
     def __init__(self):
         self.date = datetime.today()
@@ -13,6 +25,9 @@ class HistoryRecord:
 
 
 class ProcessingHistoryRecord(HistoryRecord):
+    """
+    History record for processing steps on datasets.
+    """
 
     def __init__(self):
         super().__init__()
