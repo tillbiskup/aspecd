4bf5bccf3d681668ed6ea4c2064cdf3e67b775fb
Till Biskup <till@till-biskup.de>
2018-09-09 18:09:26 +0200

    API documentation contains private methods

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,8 @@ pygments_style = 'sphinx'
 # Show or hide todo and todolist directives in output
 todo_include_todos = True
 
+# Autodoc configuration
+autodoc_default_flags = ['members', 'undoc-members', 'private-members']
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
