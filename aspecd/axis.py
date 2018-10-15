fe31aeb271759d60126b322276d2a31989f537b0
Till Biskup <till@till-biskup.de>
2018-10-15 12:23:59 +0200

    Updated documentation

--- aspecd/axis.py
+++ aspecd/axis.py
@@ -36,6 +36,31 @@ class AxisValuesTypeError(Error):
 
 
 class Axis:
+    """
+    An axis contains always both, numerical values as well as the metadata
+    necessary to create axis labels and to make sense of the numerical
+    information.
+
+    Attributes
+    ----------
+    quantity : `string`
+        quantity of the numerical data, usually used as first part of an
+        automatically generated axis label
+    unit : `string`
+        unit of the numerical data, usually used as second part of an
+        automatically generated axis label
+    label : `string`
+        manual label for the axis, particularly useful in cases where no
+        quantity and unit are provided or should be overwritten.
+
+    Raises
+    ------
+    AxisValuesTypeError
+        Raised when trying to set axis values to another type than numpy array
+    AxisValuesDimensionError
+        Raised when trying to set axis values to an array with more than one
+        dimension.
+    """
 
     def __init__(self):
         self._values = np.zeros(0)
@@ -45,6 +70,13 @@ class Axis:
 
     @property
     def values(self):
+        """
+        Get or set the numerical axis values.
+
+        Values require to be a one-dimensional numpy array. Trying to set
+        values to either a different type or a numpy array with more than one
+        dimension will raise a corresponding errors.
+        """
         return self._values
 
     @values.setter
