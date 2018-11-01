d8c0e57c8478427336f21cdd7dfb97e58031eb54
Till Biskup <till@till-biskup.de>
2018-11-01 17:57:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
-from setuptools import setup, find_packages
-
 import os
+import setuptools
 
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version_file:
     version = version_file.read().strip()
 
-with open('README.rst') as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open(os.path.join(os.path.dirname(__file__), 'LICENSE')) as f:
     license_ = f.read()
 
 
-setup(
+setuptools.setup(
     name='ASpecD',
     version=version,
     description='Framework for handling spectroscopic data.',
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Till Biskup',
     author_email='till@till-biskup.de',
     url='https://aspecd.de/',
     license=license_,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=setuptools.find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    install_requires=[
+        'numpy'
+    ],
+    python_requires='>=3',
 )
