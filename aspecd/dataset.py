Fix equidistant property in axis: use numerical accuracy

aspecd/dataset.py
@@ -1291,7 +1291,7 @@ class Axis:
         if not self.values.size:
             return
         differences = self.values[1:] - self.values[0:-1]
-        self._equidistant = (differences == differences[0]).all()
+        self._equidistant = np.isclose(differences.max(), differences.min())
 
 
 class HistoryRecord(aspecd.utils.ToDictMixin):

tests/test_dataset.py
@@ -961,7 +961,7 @@ class TestAxis(unittest.TestCase):
         self.assertEqual(self.axis.equidistant, None)
 
     def test_equidistant_is_true_for_equidistant_axes(self):
-        self.axis.values = np.arange(0, 5, 1)
+        self.axis.values = np.linspace(330, 340, num=1024, endpoint=True)
         self.assertTrue(self.axis.equidistant)
 
     def test_equidistant_is_false_for_nonequidistant_axes(self):